c6d2cff76a1901c0b17655ca3219ebc876f68a54
Dushyantjune6 <77047771+Dushyantjune6@users.noreply.github.com>
2024-03-01 13:03:12 +0530

    This is my first commit on Github.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my first code pushed on GitHub!");
+    }
+}
