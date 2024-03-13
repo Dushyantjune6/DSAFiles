eac72a16fae460f907cb687bc7db552ec7f670b6
Dushyantjune6 <77047771+Dushyantjune6@users.noreply.github.com>
2024-03-14 01:19:26 +0530

    Ceiling problem and floor using Binary Search in Java with validations

--- src/CeilingProblem.java
+++ src/CeilingProblem.java
@@ -6,6 +6,10 @@ public class CeilingProblem {
         int start=0;
         int end = arr.length-1;
 
+        if(target > arr[arr.length-1]){
+            return -1; //check to make sure value bigger than the target exists in the array
+        }
+
         while(start<=end){
             int mid = start+(end-start)/2;
             if(target > arr[mid]){
---
+++ src/FloorProblem.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class FloorProblem {
+
+    public static int floorProblemUsingBinarySearch(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length-1;
+
+        if(target < arr[0]){
+            return -1;
+        }
+
+        while(start <= end){
+            int mid = start+(end-start)/2;
+
+            if(target > arr[mid]){
+                start = mid+1;
+            }else if(target < arr[mid]){
+                end = mid-1;
+            }else{
+                return arr[mid];
+            }
+        }
+        return arr[end];
+    }
+    public static void main(String[] args){
+        int[] arr = {12,12,13,16,18,19,22,25,27,98,103,224};
+        Scanner sc = new Scanner(System.in);
+        int target =sc.nextInt();
+
+        System.out.println(floorProblemUsingBinarySearch(arr, target));
+    }
+}
