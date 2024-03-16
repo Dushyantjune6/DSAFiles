11c124b32aa88d38f0c9c7dcd40e0bb91e4d546f
Dushyantjune6 <77047771+Dushyantjune6@users.noreply.github.com>
2024-03-16 15:01:37 +0530

    Finding the index of the values when the array length is not available using the Binary search!

---
+++ src/IndexOfElementInInfiniteArray.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class IndexOfElementInInfiniteArray {
+    public static int binarySearch(int[] arr, int target, int start, int end){
+        //this method runs on the basis of start and end values shared by ans method
+        while(start <= end){
+            int mid = start+(end-start)/2;
+            if(target > arr[mid]){
+                start = mid+1;
+            }else if(target < arr[mid]){
+                end = mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+    public static int findRange(int[] arr, int target){
+        //This method will keep on doubling the size of the range unless the target comes inder it
+        //i.e. target < arr[end]
+        int start =0, end =1;
+        while(target > arr[end]){
+            int temp = end; //storing index of old end
+            end = end+(end-start+1)*2; //size of new end will be old end + (end-start+1)*2 which is 2*size of current range
+            start = temp+1; //new start will be old end+1
+        }
+        return binarySearch(arr, target, start, end);
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,11,13,14,17,19,22,24};
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        System.out.println(findRange(arr, target));
+    }
+
+}
