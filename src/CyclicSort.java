1a439758438d4e325d6c2c2483ec405aa56394be
Dushyantjune6 <77047771+Dushyantjune6@users.noreply.github.com>
2024-03-25 01:13:15 +0530

    Cyclic sort and Leetcode 448 Problem solution

---
+++ src/CyclicSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class CyclicSort {
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    public static void cycleSort(int[] arr){
+
+        int end = arr.length;
+        int index = 0;
+        while(index < end){
+            int element = arr[index];
+            int correctPosition = element-1;
+            if(arr[index] < end && arr[index] != arr[correctPosition]){
+                swap(arr,index, correctPosition);
+            }else{
+                index++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr= {4,5,6,1,2,8,3};
+        cycleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/Find_All_Numbers_Disappeared_448.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Find_All_Numbers_Disappeared_448 {
+
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    public static void cyclicSort(int[] arr){
+        int i=0;
+        while (i < arr.length){
+            int correctPosition = arr[i] -1; //arr[arr[i]] == correctPosition
+            if( arr[i] != arr[correctPosition]){
+                swap(arr, i, correctPosition);
+            }else{
+                i++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        cyclicSort(arr);
+        List<Integer> list = new ArrayList<>();
+        for(int index=0; index<arr.length; index++){
+            if( arr[index] != index+1){
+                list.add(index+1);
+            }
+        }
+        System.out.println(list);
+//        return list;
+    }
+}
