960f686ec1b0005cf55850a7eaa1569fc8f93c7a
Dushyant Malviya <77047771+Dushyantjune6@users.noreply.github.com>
2024-03-30 20:34:10 +0530

    Update FibonacciNumber.java

--- src/recursion/FibonacciNumber.java
+++ src/recursion/FibonacciNumber.java
@@ -1,4 +1,18 @@
 package recursion;
+  import java.util.*;
 
 public class FibonacciNumber {
+    public static int fibonacci(int n){
+        if(n<=1){
+            return 1;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+       
+    }
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fibonacci(n));
+	}
 }
