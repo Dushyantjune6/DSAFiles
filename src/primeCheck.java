11de57ac09bc30849ed4dc53b35754acd3252a79
Dushyant Malviya <77047771+Dushyantjune6@users.noreply.github.com>
2025-08-12 20:48:21 +0530

    Create primeCheck.java

---
+++ src/primeCheck.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class test {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPrime(11));
+    }
+}
